821c657b93206898a4d20138b605a6e7b94497f7
natsmnv <natnatanatasha@gmail.com>
2023-05-01 16:04:21 +0300

    Add index and script files

---
+++ Homework-23/script.js
@@ -0,0 +1,6 @@
+// Треба реалізувати просту програму з наступними умовами:
+// За допомогою prompt запитати ім'я користувача.
+// За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+
+let userName = prompt(`Enter your name: `);
+alert(`Hello, ${userName}! How are you?`);
