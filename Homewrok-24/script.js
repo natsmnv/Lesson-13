7b0cbd86224fb6a18a121d907411ef4ae845fd69
natsmnv <natnatanatasha@gmail.com>
2023-05-14 21:04:14 +0300

    Add index and script files

---
+++ Homewrok-24/script.js
@@ -0,0 +1,139 @@
+// Мережа фастфудів пропонує кілька видів гамбургерів:
+
+// маленький (50 тугриків, 20 калорій);
+// великий (100 тугриків, 40 калорій).
+
+// Гамбургер може бути з одним із декількох видів начинок:
+
+// сиром (+ 10 тугриків, + 20 калорій);
+// салатом (+ 20 тугриків, + 5 калорій);
+// картоплею (+ 15 тугриків, + 10 калорій).
+
+
+// Можна додати добавки:
+
+// посипати приправою (+15 тугриків, 0 калорій) - полити майонезом (+ 20 тугриків, +5 калорій).
+
+// Напишіть програму, яка розраховує вартість та калорійність гамбургера. Використовуйте ООП підхід.
+// (підказка: потрібен клас Гамбургер, константи, методи для вибору опцій та розрахунку потрібних величин)
+
+// Приклад роботи коду:
+
+// // маленький гамбургер з начинкою з сиру
+// var hamburger = new Hamburger(Hamburger .SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+// // добавка з майонезу
+// hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+// // запитаємо скільки там калорій
+// console.log(“Calories: “ + hamburger.calculate ());
+
+// // скільки коштує
+// console.log("Price: “ + hamburger.calculatePrice());
+
+// // я тут передумав і вирішив додати ще приправу
+// hamburger.addTopping(Hamburger .TOPPING_SAUCE);
+
+// // А скільки тепер коштує?
+// console.log("Price with sauce: “ + hamburger.calculatePrice());
+
+
+
+class Hamburger {
+    constructor(size, stuffing) {
+        this.size = size;
+        this.stuffing = stuffing;
+    }
+
+    static SIZE_SMALL = 'SIZE_SMALL';
+    static SIZE_BIG = 'SIZE_BIG';
+    static STUFFING_CHEESE = 'STUFFING_CHEESE';
+    static STUFFING_SALAD = 'STUFFING_SALAD';
+    static STUFFING_POTATO = 'STUFFING_POTATO';
+    static TOPPING_SAUCE = 'TOPPING_SAUCE';
+    static TOPPING_MAYO = 'TOPPING_MAYO';
+    price = 0;
+    calories = 0;
+
+    addTopping (topping) {
+        if (topping === Hamburger.TOPPING_SAUCE) {
+            this.price += 15;
+            this.calories += 0;
+        } else if (topping === Hamburger.TOPPING_MAYO) {
+            this.price += 20;
+            this.calories += 5;
+        } else {
+            console.log(`Something went wrong...`);
+        }
+    }
+
+    calculatePrice() {
+        if (this.size === Hamburger.SIZE_SMALL) {
+            this.price += 50;
+
+            if (this.stuffing === Hamburger.STUFFING_CHEESE) {
+                this.price += 10;
+            } else if (this.stuffing === Hamburger.STUFFING_SALAD) {
+                this.price += 20;
+            } else if (this.stuffing === Hamburger.STUFFING_POTATO) {
+                this.price += 15;
+            } else {
+                return `Something went wrong...`;
+            }
+        } else if (this.size === Hamburger.SIZE_BIG) {
+            this.price += 100;
+
+            if (this.stuffing === Hamburger.STUFFING_CHEESE) {
+                this.price += 10;
+            } else if (this.stuffing === Hamburger.STUFFING_SALAD) {
+                this.price += 20;
+            } else if (this.stuffing === Hamburger.STUFFING_POTATO) {
+                this.price += 15;
+            } else {
+                return `Something went wrong...`;
+            }
+        } else {
+            return `Something went wrong...`;
+        }
+        return this.price;
+    }
+
+    calculate() {
+        if (this.size === Hamburger.SIZE_SMALL) {
+            this.calories += 20;
+            
+            if (this.stuffing === Hamburger.STUFFING_CHEESE) {
+                this.calories += 20;
+            } else if (this.stuffing === Hamburger.STUFFING_SALAD) {
+                this.calories += 5;
+            } else if (this.stuffing === Hamburger.STUFFING_POTATO) {
+                this.calories += 10;
+            } else {
+                return `Something went wrong...`;
+            }
+        } else if (this.size === Hamburger.SIZE_BIG) {
+            this.calories += 40;
+
+            if (this.stuffing === Hamburger.STUFFING_CHEESE) {
+                this.calories += 20;
+            } else if (this.stuffing === Hamburger.STUFFING_SALAD) {
+                this.calories += 5;
+            } else if (this.stuffing === Hamburger.STUFFING_POTATO) {
+                this.calories += 10;
+            } else {
+                return `Something went wrong...`;
+            }
+        } else {
+            return `Something went wrong...`;
+        }
+        return this.calories;
+    }
+}
+
+let burger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+burger.addTopping(Hamburger.TOPPING_SAUCE);
+console.log(`Price: ${burger.calculatePrice()}`);
+console.log(`Calories: ${burger.calculate()}`);
+burger.addTopping(Hamburger.TOPPING_MAYO);
+console.log(`Price: ${burger.calculatePrice()}`);
+console.log(`Calories: ${burger.calculate()}`);
